5bca833bc2c87cec4e54989184a65eaa884ac136
dawn <78233879+dawnofmidnight@users.noreply.github.com>
2023-02-19 15:31:59 -0500

    lang: establish foundation & lexer (#1)

--- crates/selene-lang/src/lib.rs
+++ crates/selene-lang/src/lib.rs
@@ -1,3 +1,28 @@
 //! The Selene language.
 
+#![feature(lint_reasons, once_cell)]
 #![warn(clippy::pedantic, rust_2018_idioms)]
+#![expect(
+    clippy::must_use_candidate,
+    reason = "as an application, there's not a lot of value in this"
+)]
+
+pub mod error;
+mod span;
+mod ty;
+mod value;
+
+use error::ErrorSink;
+
+/// Execute the given source code as a whole.
+///
+/// TODO: This is a placeholder for now. Eventually execution will have a more
+/// interactive API.
+///
+/// # Errors
+///
+/// If any errors are encountered, they will be returned as a `Result::Err`.
+/// Otherwise, execution can be assumed to have succeeded.
+pub fn execute(_source: &str) -> Result<(), ErrorSink> {
+    Ok(())
+}
